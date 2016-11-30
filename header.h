43945f9ca778ae89e6fbb5eb90cbea71fb987fea
cdevy <c.devy95@gmail.com>
2016-11-30 15:55:52 +0100

    modif header

--- header.h
+++ header.h
@@ -2,29 +2,29 @@
 
 
 typedef struct header_posix_ustar {
-char name[100];
-char mode[8];
-char uid[8];
-char gid[8];
-char size[12];
-char mtime[12];
-char checksum[8];
-char typeflag[1];
-char linkname[100];
-char magic[6];
-char version[2];
-char uname[32];
-char gname[32];
-char devmajor[8];
-char devminor[8];
-char prefix[155];
-char pad[12];
+  char name[100];
+  char mode[8];
+  char uid[8];
+  char gid[8];
+  char size[12];
+  char mtime[12];
+  char checksum[8];
+  char typeflag[1];
+  char linkname[100];
+  char magic[6];
+  char version[2];
+  char uname[32];
+  char gname[32];
+  char devmajor[8];
+  char devminor[8];
+  char prefix[155];
+  char pad[12];
 } header_t;
 
 typedef struct pile_header {
-struct header_posix_ustar* header;
-int pos;
-int size;
-char path[257];
-struct pile_header* next;
+  struct header_posix_ustar* header;
+  int pos;
+  int size;
+  char path[257];
+  struct pile_header* next;
 } pile_h;
